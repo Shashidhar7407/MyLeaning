3f0945c96c445e51104da955947e4f8b38b81d31
Shashidhar Papishetty <64946243+Shashidhar7407@users.noreply.github.com>
2020-09-06 12:36:10 +0530

    Create highest_ffibbonacci.py
    highest number in the fibbonacci

---
+++ highest_ffibbonacci.py
@@ -0,0 +1,19 @@
+# program to find the highest number in the series
+x=0
+y=1
+n=int(input("enter the number of to find the highest fibbonacci series"))
+i=0
+if(n<=0):
+    print("invalid input")
+elif(n==1):
+    print(1)
+else:
+    # print(x)
+    # print(y)
+    for i in range(n-2):
+        z=x+y
+        temp=x
+        x=y 
+        y=z
+        i+=1
+print(z)
